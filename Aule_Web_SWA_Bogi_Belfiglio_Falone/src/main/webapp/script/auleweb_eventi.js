8f51e09a7abf0ca786dea3f563acc242f6bc3618
Stefano Belfiglio <83875374+stefanobelfiglio@users.noreply.github.com>
2023-07-07 18:04:48 +0200

    correzione errori

--- Aule_Web_SWA_Bogi_Belfiglio_Falone/src/main/webapp/script/auleweb_eventi.js
+++ Aule_Web_SWA_Bogi_Belfiglio_Falone/src/main/webapp/script/auleweb_eventi.js
@@ -31,40 +31,6 @@ function getCurrentEventi() {
     });
 }
 
-function showEventInformationsByName() {
-    let form = document.getElementById("search_evento");
-    let search = form.elements["searchEvento"].value;
-    $.ajax({
-        url: "rest/eventi/" + search,
-        method: "GET",
-        success: function (response) {
-            $("#popupEvento").empty();
-            let eventContent =
-                    "<h2>INFORMAZIONI</h2>" +
-                    "<p>nome: " + response["nome"].toLowerCase() + "</p>" +
-                    "<p>data: " + response["data"] + "</p>" +
-                    "<p>intervallo: " + response["ora_inizio"] + " - " + response["ora_fine"] + "</p>" +
-                    "<p>aula: " + response["aula"].toLowerCase() + "</p>" +
-                    "<p>responsabile: " + response["responsabile"].toLowerCase() + "</p>" +
-                    "<p>tipologia :" + response["tipo"].toLowerCase() + "</p>";
-            if (response["tipo"] === "LEZIONE" || response["tipo"] === "ESAME" || response["tipo"] === "PARZIALE") {
-                eventContent += "<p>corso: " + response["corso"].toLowerCase() + "</p>";
-            }
-            eventContent += "<p>ricorrenza: " + response["ricorrenza"].toLowerCase() + "</p>";
-            if (response["ricorrenza"] !== "NESSUNA") {
-                eventContent += "<p>intervallo ricorrenza: " + response["data"] + " - " + response["data_ricorrenza"] + "</p>";
-            }
-
-            $("#popupEvento").append(eventContent);
-            $("#popupEvento").fadeIn(1000);
-        },
-        error: function (xhr, status, error) {
-            $("#tableEventi").append(xhr.responseText);
-        }
-    });
-}
-
-
 //funzione per visualizzazione informazioni evento
 function showEventInformations(id) {
     $.ajax({
@@ -147,7 +113,7 @@ function showEventInformationsByName() {
                         '<button type="button" onclick="fadeOutPopupEvento()">X</button>' +
                         '</div>' +
                         '<h2  style="clear: right; color: red">Evento non trovato</h2>' +
-                        '<p>Non è stato trovato alcun evento con nome "'+ search +'"</p>';
+                        '<p>Non è stato trovato alcun evento con nome "' + search + '"</p>';
                 $("#popupEvento").append(eventContent);
                 $("#popupEvento").fadeIn(1000);
             }
@@ -155,6 +121,16 @@ function showEventInformationsByName() {
         },
         error: function (xhr, status, error) {
             $("#tableEventi").append(xhr.responseText);
+            $("#popupEvento").empty();
+            let eventContent =
+                    '<div>' +
+                    '<div class="exit">' +
+                    '<button type="button" onclick="fadeOutPopupEvento()">X</button>' +
+                    '</div>' +
+                    '<h2  style="clear: right; color: red">Evento non trovato</h2>' +
+                    '<p>Non sono permessi caratteri speciali per la ricerca di un evento</p>';
+            $("#popupEvento").append(eventContent);
+            $("#popupEvento").fadeIn(1000);
 
         }
     });
